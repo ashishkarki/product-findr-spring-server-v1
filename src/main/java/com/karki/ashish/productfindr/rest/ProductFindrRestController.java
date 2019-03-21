8e1da813a2cb6fd471ca665334bbd7d40219c20e
Ashish Karki <u2aashish@gmail.com>
2019-03-20 21:12:55 -0400

    Adding global exception handler instead of local controller ex. handler.

--- src/main/java/com/karki/ashish/productfindr/rest/ProductFindrRestController.java
+++ src/main/java/com/karki/ashish/productfindr/rest/ProductFindrRestController.java
@@ -5,16 +5,12 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.karki.ashish.productfindr.entity.Product;
-import com.karki.ashish.productfindr.error.ProductErrorResponse;
 import com.karki.ashish.productfindr.error.ProductNotFoundException;
 
 // @CrossOrigin(methods= {})
@@ -53,26 +49,5 @@ public class ProductFindrRestController {
 		}
 
 		return products.get(productId); // keep it simple for now
-	}
-
-	@ExceptionHandler
-	public ResponseEntity<ProductErrorResponse> handleException(ProductNotFoundException pnfe) {
-		ProductErrorResponse errorResponse = new ProductErrorResponse();
-		errorResponse.setMessage(pnfe.getMessage());
-		errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-		errorResponse.setTimestamp(System.currentTimeMillis());
-
-		return new ResponseEntity<ProductErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
-	}
-	
-	// catch all exception handling
-	@ExceptionHandler
-	public ResponseEntity<ProductErrorResponse> handleException(Exception e) {
-		ProductErrorResponse errorResponse = new ProductErrorResponse();
-		errorResponse.setMessage(e.getMessage());
-		errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-		errorResponse.setTimestamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<ProductErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 }
